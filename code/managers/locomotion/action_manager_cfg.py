37baa808020db27e7a5efce763336e44c3bbea90
filippottaviani <filippo.ottaviani.tr@gmail.com>
2025-07-03 17:24:39 +0200

    Aggiunta controllori vel e pos

--- code/managers/locomotion/action_manager_cfg.py
+++ code/managers/locomotion/action_manager_cfg.py
@@ -1,26 +1,28 @@
 from isaaclab.utils import configclass
-from isaaclab.envs.mdp import JointEffortActionCfg
+from isaaclab.envs.mdp import JointEffortActionCfg, JointVelocityActionCfg, JointPositionActionCfg
 import torch
 
 @configclass
 class ActionsCfg:
 
-    joint_efforts = JointEffortActionCfg(
+    '''joint_efforts = JointEffortActionCfg(
         asset_name="robot", 
-        joint_names=[
-            "left_hip_pitch_joint",
-            "left_hip_roll_joint",
-            "left_hip_yaw_joint",
-            "left_knee_joint",
-            "left_ankle_pitch_joint",
-            "left_ankle_roll_joint",
-            "right_hip_pitch_joint",
-            "right_hip_roll_joint",
-            "right_hip_yaw_joint",
-            "right_knee_joint",
-            "right_ankle_pitch_joint",
-            "right_ankle_roll_joint"
-        ],
-        scale= 0.5
+        joint_names=[".*"],
+        scale= 0.5,
+        use_default_offset=True
+    )'''
+
+    '''joint_efforts = JointVelocityActionCfg(
+        asset_name="robot", 
+        joint_names=[".*"],
+        scale= 0.5,
+        use_default_offset=True
+    )'''
+
+    joint_pos = JointPositionActionCfg(
+        asset_name="robot", 
+        joint_names=[".*"], 
+        scale=0.5, 
+        use_default_offset=True
     )
 
